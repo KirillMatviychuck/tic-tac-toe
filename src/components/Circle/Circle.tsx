1fb524a15df2177d536562bb7b0580a12e983e03
Kirill <k.matviychuck@gmail.com>
2024-02-19 10:42:40 +0200

    add sass, changes in index.html

---
+++ src/components/Circle/Circle.tsx
@@ -0,0 +1,7 @@
+export const Circle = () => {
+    return (
+        <>
+            Circle
+        </>
+    )
+}
---
+++ src/components/Cross/Cross.tsx
@@ -0,0 +1,7 @@
+export const Cross = () => {
+    return (
+        <>
+            Cross
+        </>
+    )
+}
